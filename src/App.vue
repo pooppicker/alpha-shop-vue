872fb3a598c8c26dc7797f4f5c351e6195f59014
Wendy Tsai <>
2021-08-30 16:48:27 +0800

    create Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div id="alpha-shop">
+      <Navbar />
+    </div>
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Navbar from "./components/Navbar";
 
 export default {
-  name: "App",
   components: {
-    HelloWorld,
+    Navbar,
   },
 };
 </script>
 
 <style>
 #app {
+  height: 100%;
+  width: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.nav {
+  width: 100%;
+  height: 60px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
+.nav__menu,
+.nav__brand,
+.nav__icons {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.nav__menu {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.nav__menu a {
+  text-decoration: none;
+  color: #2a2a2a;
+}
+.nav__menu li {
+  text-decoration: none;
+  list-style: none;
+  display: inline-block;
+  padding: 0 10px;
+}
+.nav__brand {
+  position: absolute;
+  top: 1rem;
+  left: 50%;
+  transform: translateX(-50%);
+}
+.nav__brand .ac-logo {
+  width: 10rem;
+}
+.nav__icons .icon {
+  margin: 0 1.4rem;
+  width: 1.4rem;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav class="nav">
+    <ul class="nav__menu">
+      <li class="nav__item"><a class="nav__link" href="#">男款</a></li>
+      <li class="nav__item"><a class="nav__link" href="#">女款</a></li>
+      <li class="nav__item"><a class="nav__link" href="#">最新消息</a></li>
+      <li class="nav__item"><a class="nav__link" href="#">聯絡我們</a></li>
+      <li class="nav__item"><a class="nav__link" href="#">客製商品</a></li>
+    </ul>
+    <a class="nav__brand" href="#"
+      ><img
+        class="ac-logo"
+        src="https://i.ibb.co/SPbbHH0/Logo-2x.jpg"
+        alt="AC logo"
+    /></a>
+    <div class="nav__icons">
+      <a href="#"
+        ><img class="icon" src="https://i.ibb.co/4JLfqgS/search-2x.png" alt=""
+      /></a>
+      <a href="#"
+        ><img class="icon" src="https://i.ibb.co/qm1FbGf/cart-2x.png" alt=""
+      /></a>
+      <a href="#"
+        ><img class="icon" src="https://i.ibb.co/Lg5j4xY/moon-2x.png" alt=""
+      /></a>
+    </div>
+  </nav>
+</template>
