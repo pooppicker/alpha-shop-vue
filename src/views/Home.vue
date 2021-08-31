19180ba60a4d8ccb65f9fd57ee9305c422054ba0
Wendy Tsai <>
2021-08-31 10:54:21 +0800

    feat: add buttons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,12 @@
     <div class="col-md-6">
       <Steppers />
     </div>
+    <div id="btn-control" class="main__button">
+      <button class="btn main__button__previous" disabled="disabled">
+        上一步
+      </button>
+      <button class="btn main__button__next">下一步</button>
+    </div>
   </div>
 </template>
 
@@ -29,6 +35,44 @@ export default {
   align-items: flex-start;
   margin: 20px auto;
   position: relative;
-  outline: 1px solid black;
+  /* outline: 1px solid black; */
+}
+.main__button {
+  position: absolute;
+  top: 100%;
+  margin-top: 2rem;
+  width: 60%;
+  border-top: 1px solid #f0f0f5;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.main__button .btn {
+  font-size: 14px;
+  cursor: pointer;
+}
+.main__button__next {
+  border: none;
+  border-radius: 5px;
+  height: 2.5rem;
+  padding: 0 3rem;
+  background-color: #f67599;
+  color: #ffffff;
+}
+.main__button__next::after {
+  content: "→";
+  margin-left: 1rem;
+}
+.main__button__previous {
+  -webkit-appearance: none;
+  border: none;
+  background: white;
+}
+/* .main__button__previous[disabled] {
+  visibility: hidden;
+} */
+.main__button__previous::before {
+  content: "←";
+  margin-right: 1rem;
 }
 </style>
